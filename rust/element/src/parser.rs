20fa97af8a39c29c868a3ae9d8b9940709beba14
scarab5q <jackmdenny@gmail.com>
2019-05-02 14:22:07 +0100

    addefs GPLv3 headers to all the source files (#20)

--- rust/element/src/parser.rs
+++ rust/element/src/parser.rs
@@ -1,3 +1,18 @@
+//    This file is part of org-rs.
+//
+//    org-rs is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    org-rs is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with org-rs.  If not, see <https://www.gnu.org/licenses/>.
+
 extern crate xi_rope;
 
 use crate::cursor::CursorHelper;
