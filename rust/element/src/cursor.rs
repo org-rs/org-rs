e1582753dfe79edc01b219add0e63e923cd003eb
Taylor <taylor@oddball.io>
2019-06-08 16:38:55 -0600

    Add search-forward and skip-chars-forward functions (#36)
    * search-forward 
    * skip-chars-forward

--- rust/element/src/cursor.rs
+++ rust/element/src/cursor.rs
@@ -363,7 +363,85 @@ impl<'a> Cursor<'a> {
             LinesMetric::is_boundary(self.data, self.pos)
         }
     }
+
+    /// Search forward from point to str. Sets point to the end of the
+    /// occurence found and returns point. bound is a position in the
+    /// buffer. The match found must not end after that position. If
+    /// None then search to end of the buffer. If count is specified,
+    /// find the countth occurence. If countth occurence is not found
+    /// None is returned. If count is not provided then 1 is used as
+    /// count. Note that searching backward is not supported like it
+    /// is in the elisp equivalent.
+    pub fn search_forward(&mut self, str: &str,
+                          bound: Option<usize>,
+                          count: Option<usize>) -> Option<usize> {
+        let count = match count {
+            Some(count) => count,
+            _ => 1
+        };
+
+        let bound = match bound {
+            Some(bound) => bound,
+            _ => self.data.len()
+        };
+
+        let pos = self.pos();
+        if bound < pos {
+            return None;
+        }
+
+        let mut iter = self.data[pos..].match_indices(str);
+        let mut i = 1;
+        loop {
+            match iter.next() {
+                Some(result) => {
+                    if result.0 + pos + str.len() > bound {
+                        return None;
+                    }
+
+                    if count == i {
+                        self.set(result.0 + pos + str.len());
+                        return Some(result.0 + pos + str.len());
+                    }
+
+                    i += 1;
+                },
+                None => {
+                    return None
+                }
+            }
+        }
+    }
+
+    /// Moves point forward, stopping before a char not in str, or at position limit.
+    pub fn skip_chars_forward(&mut self, str: &str, limit: Option<usize>) -> usize {
+        let pos = self.pos();
+        let limit = match limit {
+            Some(lim) => lim,
+            _ => self.data.len()
+        };
+        
+        if pos >= limit {
+            return 0;
+        }
+
+        let mut count = 0;
+        while let Some(c) = self.get_next_char() {
+            if !str.contains(c) {
+                self.get_prev_char();
+                return count;
+            }
+            if count + pos > limit {
+                self.get_prev_char();
+                return count;
+            }
+            count += 1;
+        }
+        count
+    }
 }
+
+
 
 /// Given the inital byte of a UTF-8 codepoint, returns the number of
 /// bytes required to represent the codepoint.
@@ -590,5 +668,34 @@ mod test {
         assert!(cursor.is_bol());
         cursor.goto_next_line();
         assert!(cursor.is_bol());
+    }
+
+    #[test]
+    fn search_forward() {
+        let str = "onetwothreefouronetwothreeonetwothreeonetwothreefouroneabababa";
+        let mut cursor = Cursor::new(&str, 0);
+        assert_eq!(cursor.search_forward("one", None, Some(2)), Some(18));
+        assert_eq!(cursor.search_forward("one", None, None), Some(29));
+        cursor.set(0);
+        assert_eq!(cursor.search_forward("threeone", Some(10), None), None); // there is no match before 10th pos
+        assert_eq!(cursor.search_forward("threeone", Some(100), Some(10)), None); // there is not a 10th match so return None
+        assert_eq!(cursor.search_forward("two", None, Some(4)), Some(43));
+        assert_eq!(cursor.pos(), 43);
+        assert_eq!(cursor.search_forward("aba", Some(10), None), None); // bound is before current pos
+        assert_eq!(cursor.pos(), 43);
+        assert_eq!(cursor.search_forward("aba", Some(10000), Some(2)), Some(62));
+        cursor.set(0);
+        assert_eq!(cursor.search_forward("aba", Some(10000), Some(6)), None);
+    }
+
+    #[test]
+    fn skip_chars_forward() {
+        let str = "  k\t **hello";
+        let mut cursor = Cursor::new(&str, 0);
+        assert_eq!(cursor.skip_chars_forward(" ", None), 2);
+        assert_eq!(cursor.pos(), 2);
+        assert_eq!(cursor.skip_chars_forward(" k\t", None), 3);
+        cursor.set(0);
+        assert_eq!(cursor.skip_chars_forward("* k\t", Some(2)), 3);
     }
 }
