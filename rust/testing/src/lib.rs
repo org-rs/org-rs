7fb31a7fa0b29f06bf136c9cc51da890ed545751
Asa Zeren <asaizeren@gmail.com>
2020-10-19 15:01:02 -0400

    Add the `testing` crate
    There are a number of important subroutines for testing a number of org
    features, not just org-element, so these have to live in a different crate.
    
    This crate is called `testing` as (a) it is a reasonable name, and (b) it is
    the name of the corrosponding folder in the org source.

---
+++ rust/testing/src/lib.rs
@@ -0,0 +1,9 @@
+//! # Testing
+//!
+//! This module should contain test helpers. Most of these are going
+//! to be found in [`org-test.el`]. Note that this is not a complete
+//! port of `org-test.el`, as the interactive components are out of
+//! scope for this project, being a part of the org-emacs integration,
+//! not a fundumental part of org.
+//!
+//! [`org-test.el`]: https://code.orgmode.org/bzg/org-mode/src/master/testing/org-test.el
