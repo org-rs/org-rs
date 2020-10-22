7ec4e36ec2b52765f7f7ee59fe701f7c32d9554f
Asa Zeren <asaizeren@gmail.com>
2020-10-21 22:36:29 -0400

    Implement `SyntaxNode::append_child`
    Adds a convient method to add a child to a syntax node. This should improve the
    readability of code that constructs trees of `SyntaxNode`s.

--- rust/element/src/data.rs
+++ rust/element/src/data.rs
@@ -98,6 +98,13 @@ impl<'a> SyntaxNode<'a> {
             affiliated: None,
         }
     }
+
+    /// Appends a child to the node, setting the child's parent correctly.
+    pub fn append_child(self: Handle<'a>, child: Handle<'a>) {
+        *child.parent.borrow_mut() = Some(Rc::downgrade(&self));
+        self.children.borrow_mut().push(child);
+    }
+    
 }
 
 /// Complete list of syntax entities
@@ -112,6 +119,13 @@ pub enum Syntax<'a> {
 
     /// Greater element
     CenterBlock,
+
+
+
+
+
+
+
 
     /// Element
     Clock(Box<ClockData<'a>>),
@@ -879,5 +893,4 @@ mod test {
         assert!(closure_test(br, |that| bold.can_contain(that)));
         assert!(!closure_test(verse, |that| bold.can_contain(that)));
     }
-
 }
